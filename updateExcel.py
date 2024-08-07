cdaef02728aee4075c6ffa2d9ce4bc116c0d1bdd
Diego Cavalcanti <diego.scavalcanti@gmail.com>
2024-08-07 14:23:29 -0300

    convertido para xlwings

--- updateExcel.py
+++ updateExcel.py
@@ -1,10 +1,8 @@
-import openpyxl
 import os
-from datetime import datetime
+import sys
+import xlwings as xw
 
-ativos = ["BTG CDB Plus FIRF CrPr","16,10% a.a.", "15,86% a.a.", "12,79% a.a.", "IPCA + 6,20%", "13,58% a.a.","IPCA + 9,46%","IPCA + 9,11%", "BTG PACT CRED CORP II PREV FC FI MULT CP","EXES PREV FIE FC FI MULT CRED PRIV", "Conta corrente"]
-
-def atualizar_celula_com_data():
+def atualizar_celulas():
     def obter_nome_arquivo():
         while True:
             nome_arquivo = input("Digite o nome do arquivo Excel : ") + ".xlsx"
@@ -13,56 +11,85 @@ def atualizar_celula_com_data():
                 print(f"Arquivo '{nome_arquivo}' encontrado.")
                 return caminho_arquivo
             else:
-                print(f"O arquivo '{nome_arquivo}' não foi encontrado. Por favor, tente novamente.")
-    
+                print(f"O arquivo '{nome_arquivo}' não foi encontrado.")
+                resp = int(input("Deseja tentar novamente? (1 - Sim, 0 - Não): "))
+                if resp == 0:
+                    print("Encerrando o programa.")
+                    sys.exit()
+
     caminho_arquivo = obter_nome_arquivo()
 
     try:
-        # Abre o arquivo Excel existente
-        workbook = openpyxl.load_workbook(caminho_arquivo)
+        # Abre o arquivo Excel usando xlwings
+        workbook = xw.Book(caminho_arquivo)
 
-        # Obtém a planilha chamada 'Dados'
         def obter_planilha(workbook):
             while True:
-                nome_planilha = input("Digite o nome da planilha (ex: Dados): ")
-                if nome_planilha in workbook.sheetnames:
-                    sheet = workbook[nome_planilha]
+                print("Planilhas disponíveis: ", [sheet.name for sheet in workbook.sheets])
+                nome_planilha = input("Digite o nome exato da planilha desejada (ex: Dados): ")
+                planilhas = [sheet.name for sheet in workbook.sheets]
+                if nome_planilha in planilhas:
                     print(f"Planilha '{nome_planilha}' encontrada.")
-                    return sheet
+                    return nome_planilha
                 else:
-                    print(f"A planilha '{nome_planilha}' não foi encontrada. Por favor, tente novamente.")
-        
-        sheet = obter_planilha(workbook)
+                    print(f"A planilha '{nome_planilha}' não foi encontrada.")
+                    resp = int(input("Deseja tentar novamente? (1 - Sim, 0 - Não): "))
+                    if resp == 0:
+                        print("Encerrando o programa.")
+                        sys.exit()
 
-        # Solicitar a posição da primeira célula líquida
-        primeira_celula_liquida = input("Digite a posição da primeira célula líquida (ex: Z6): ")
+        nome_planilha = obter_planilha(workbook)
+        sheet = workbook.sheets[nome_planilha]
 
-        # Separar a coluna e a linha da célula fornecida
-        col, row = openpyxl.utils.cell.coordinate_from_string(primeira_celula_liquida)
-        row = int(row)  # Converter a linha para inteiro
+        resp = 1
+        while resp == 1:
+            print("[1] - Atualizar ativos líquidos")
+            print("[2] - Atualizar ativos brutos")
+            opcao = int(input("Digite a opção desejada: "))
+            match opcao:
+                case 1:
+                    # Solicite a posição da primeira e última célula
+                    primeira_celula_liquida = input("Digite a posição do primeiro ativo líquido (ex: B5): ")
+                    ultima_celula_liquida = input("Digite a posição do último ativo líquido (ex: B10): ")
 
-        # Atualizar valores na ordem da lista de ativos
-        def atualiza_valor_ordenado(ativos, sheet, col, row):
-            for i in range(len(ativos)):
-                # Calcular a célula líquida e a célula bruta para o ativo atual
-                cell_liquida = f"{col}{row + i}"
-                cell_bruto = f"{openpyxl.utils.cell.get_column_letter(openpyxl.utils.cell.column_index_from_string(col) + 1)}{row + i}"
+                    # Converta as coordenadas das células
+                    coluna_primeira = primeira_celula_liquida[0]
+                    linha_primeira = int(primeira_celula_liquida[1:])
+                    coluna_ultima = ultima_celula_liquida[0]
+                    linha_ultima = int(ultima_celula_liquida[1:])
 
-                # Pedir os valores do usuário
-                valor_liquido = input(f"Digite o valor do {ativos[i]} líquido: ")
-                valor_bruto = input(f"Digite o valor do {ativos[i]} bruto: ")
+                    # Verifique se as colunas são iguais
+                    if coluna_primeira != coluna_ultima:
+                        raise ValueError("As colunas das células inicial e final devem ser iguais.")
 
-                # Atribuir os valores às células
-                sheet[cell_liquida].value = valor_liquido
-                sheet[cell_bruto].value = valor_bruto
+                    # Calcule a quantidade de linhas
+                    quantidade_linhas = linha_ultima - linha_primeira + 1
+                    valores = []
 
-        atualiza_valor_ordenado(ativos, sheet, col, row)
+                    # Capture os valores das células no intervalo especificado
+                    for i in range(quantidade_linhas):
+                        cell_value = sheet.range(f'{coluna_primeira}{linha_primeira + i}').value
+                        valores.append(cell_value)
 
-        # Salva o arquivo Excel com as alterações
-        workbook.save(caminho_arquivo)
+                    primeiro_valor_ativo = input("Digite a posição da célula inicial (ex: B6): ")
+                    coluna_ativo = primeiro_valor_ativo[0]
+                    linha_inicial = int(primeiro_valor_ativo[1:])
+
+                    # Percorra a lista e peça inputs ao usuário para cada célula
+                    for i in range(len(valores)):
+                        celula_atual = f'{coluna_ativo}{linha_inicial + i}'
+                        valor = input(f"Digite o valor para {valores[i]}: ")
+                        sheet.range(celula_atual).value = valor
+
+                case _:
+                    print("Opção inválida.")
+                    
+            resp = int(input("Deseja tentar novamente? (1 - Sim, 0 - Não): "))
+
+        workbook.save()
         print('Informação adicionada com sucesso!')
     except Exception as error:
         print('Erro ao atualizar célula:', error)
 
-# Chama a função para atualizar a célula com a data
-atualizar_celula_com_data()
+# Chama a função para atualizar as células
+atualizar_celulas()
